e0c95938b931a1ca2f8ca2035e953cc8e8ed312d
Kanimozhi Mahendran <109454570+Kanimozhi26@users.noreply.github.com>
2022-10-09 13:09:35 +0530

    Add files via upload

---
+++ SpringBootAwsApplication.java
@@ -0,0 +1,19 @@
+package com.springbootaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootAwsApplication {
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to AWS First Deployment...DEPLOY APPLICATION ON CLOUD";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsApplication.class, args);
+	}
+
+}
