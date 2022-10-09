801d0992b02c022c4c9f8d3ede2c9e106a4d069f
Kanimozhi Mahendran <109454570+Kanimozhi26@users.noreply.github.com>
2022-10-09 13:10:19 +0530

    Add files via upload

---
+++ SpringBootAwsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.springbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
